bc298c6c68b19052d7610cf749270c624ba325f1
Roy Barber <roy@wearedamage.com>
2018-09-20 13:31:27 +0100

    first commit

---
+++ json/variables.js
@@ -0,0 +1,1 @@
+{"logo_url":"/img/logo.png","company_name":"Lawrence Property","page_title":"Lawrence Property - Build * Buy * Invest","page_description":"Lawrence Property your partner in investment property.","site_name":"LarewnceProperty","year":"2018","twitter":"wearedamage"}
